bb46d90a47428169df39e80ab4421058e4ad40be
PATRICK <patrickajoseph.projects@gmail.com>
2020-04-19 08:46:59 +0530

    Just a simple program to demonstrate input feature of Python | Uploaded on 08:46, 19 / 04 / 2020

---
+++ name.py
@@ -0,0 +1,6 @@
+name = input()
+print(f"hello,{name}!")
+
+#f stands for format string and works only in python 3
+#To run python 3 programs in ubuntu terminal, do as follows:
+# 		python3 <filename>.py
